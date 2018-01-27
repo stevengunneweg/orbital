1a1def9b15eb6922aa746676a2781d456290eb7e
Kay <kay.goossen@gmail.com>
2018-01-27 16:32:31 +0100

    BUG FIX: The function to get the difference in angle between two angles is now working

--- Assets/Scripts/MathUtils.cs
+++ Assets/Scripts/MathUtils.cs
@@ -9,9 +9,9 @@ public static class MathUtils
     public static readonly float RADIUS2DEGREE = 180 / Mathf.PI;
     public static float AngleDifference(float angleA, float angleB)
     {
-        float difference = (angleA - angleB + 360) % 180;
+        float difference = (angleA - angleB + 360) % 360;
         if (difference > 180)
-            difference = (difference + 180)%360;
+            difference = 360 - difference;
         return difference;
     }
 }
