892c648738771b49b6a9442326609fee10ceea3f
Goosfrabix <goosfraba05@hotmail.com>
2018-01-27 04:03:23 +0100

    Added a 3D Earth and postproccessing assets

---
+++ Assets/Scripts/Earth/SimpleScalePulse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleScalePulse : MonoBehaviour {
+
+    Vector3 scale;
+    [SerializeField]
+    float magnitude = 0.1f;
+    [SerializeField]
+    float speed = 50;
+
+	// Use this for initialization
+	void Start () {
+        scale = transform.localScale;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = scale * (1+(Mathf.Sin(Time.time * speed) * magnitude));
+	}
+}
