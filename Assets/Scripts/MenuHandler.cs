77c09fbf8dca1020e6a29d52faf02e3911e56229
Patrick Scheper <patrick.scheper@outlook.com>
2018-01-27 04:06:57 +0100

    Add menu scene

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour {
+
+    public delegate void BuySatelliteEvent();
+    public static event BuySatelliteEvent OnBuySattelite;
+	public void BuySatellite()
+    {
+        if (OnBuySattelite != null)
+        {
+            OnBuySattelite();
+        }
+    }
+}
