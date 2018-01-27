dbf3a6ecef484d1cfacbe8fbffd501b224257aa2
Kay <kay.goossen@gmail.com>
2018-01-27 10:20:06 +0100

    Added SignalTransmitter to the satalites, added people and added score

---
+++ Assets/Scripts/MathUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public static class MathUtils
+{
+    public static readonly float RADIUS2DEGREE = 180 / Mathf.PI;
+    public static float AngleDifference(float angleA, float angleB)
+    {
+        float difference = (angleA - angleB + 360) % 180;
+        if (difference > 180)
+            difference -= 180;
+        return difference;
+    }
+}
---
+++ Assets/Scripts/SignalTransmitter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalTransmitter : MonoBehaviour
+{
+    static Dictionary<SignalType, List<SignalTransmitter>> instances;
+    public static Dictionary<SignalType, List<SignalTransmitter>> Instances
+    {
+        get
+        {
+            if (instances == null)
+            {
+                instances = new Dictionary<SignalType, List<SignalTransmitter>>();
+                foreach (SignalType type in Enum.GetValues(typeof(SignalType)))
+                    instances.Add(type, new List<SignalTransmitter>());
+            }
+            return instances;
+        }
+    }
+    
+    public HashSet<SignalReceiver> Receivers { get; private set; }
+
+    [SerializeField]
+    double broadcastRadius = 20; //In degree
+    [SerializeField]
+    SignalType signalType;
+    public SignalType SignalType { get { return signalType; } }
+
+    Transform planet;
+
+	// Use this for initialization
+	void Start () {
+        Receivers = new HashSet<SignalReceiver>();
+
+        Instances[signalType].Add(this);
+        if (planet == null)
+        {
+            GameObject planetGameObject = GameObject.Find("Earth");
+            if (planetGameObject != null)
+                this.planet = planetGameObject.transform;
+        }
+	}
+
+    void OnDestroy()
+    {
+        Instances[signalType].Remove(this);
+    }
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        Receivers = findConnectedReceivers();        
+    }
+
+    public bool ReceivesSignal(SignalReceiver receiver)
+    {
+        Vector2 planetDirection = (Vector2)planet.position - (Vector2)transform.position;
+        if (Vector2.Dot(planetDirection, (Vector2)planet.position - (Vector2)receiver.transform.position) < 0)
+            return false;
+        
+        Vector2 receiverDirection = (Vector2)receiver.transform.position - (Vector2)transform.position;
+
+        float planetAngle = Mathf.Atan2(planetDirection.y, planetDirection.x)*MathUtils.RADIUS2DEGREE;
+        float receiverAngle = Mathf.Atan2(receiverDirection.y, receiverDirection.x)*MathUtils.RADIUS2DEGREE;
+
+        if (Mathf.Abs(MathUtils.AngleDifference(planetAngle, receiverAngle)) > broadcastRadius)
+            return false;
+
+        return true;
+    }
+    
+    HashSet<SignalReceiver> findConnectedReceivers()
+    {
+        HashSet<SignalReceiver> connectedReceivers = new HashSet<SignalReceiver>();
+        foreach (var receiver in SignalReceiver.Instances)
+            if (ReceivesSignal(receiver))
+                connectedReceivers.Add(receiver);
+        return connectedReceivers;
+    }
+}
