da5883077ee0626d8f765b3243df2d2a07b9d57e
Kay <kay.goossen@gmail.com>
2018-01-28 01:45:25 +0100

    Friendly ships have bullets labeled as friendly

--- Assets/Scripts/Modules/Attack.cs
+++ Assets/Scripts/Modules/Attack.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Satelite))]
 public class Attack : MonoBehaviour {
 
     public float angle;
@@ -9,12 +10,16 @@ public class Attack : MonoBehaviour {
     public float reloadDuration = 1f;
     public GameObject bulletPrefab;
 
+    private bool isPlayer { get { return satelite.IsPlayer; } }
+
     private Timer isReloading;
 
     private BulletParent bulletParent;
+    private Satelite satelite;
 
     protected void Start()
     {
+        satelite = GetComponent<Satelite>();
         angle = Random.value * 360f;
         bulletParent = Hierarchy.GetComponentWithTag<BulletParent>();
         isReloading = new Timer(reloadDuration);
@@ -37,6 +42,7 @@ public class Attack : MonoBehaviour {
             // Create the bullet
             var bulletInstance = Instantiate(bulletPrefab, transform.position + bulletDirection * 0.2f, Quaternion.identity, bulletParent.transform);
             var bullet = bulletInstance.GetComponent<Bullet>();
+            bullet.isPlayer = isPlayer;
 
             // Apply the bullet direction to the bullet
             bullet.SetDirection(bulletDirection);
